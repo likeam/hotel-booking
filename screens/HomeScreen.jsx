a2cec4474c4cc3eebe589b2ae635925b71d8c0e1
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-09 12:27:53 +0500

    Buildig calender

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,8 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, Feather } from "@expo/vector-icons";
 import Header from "../components/Header";
+import DatePicker from "react-native-date-ranges";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -34,9 +42,70 @@ const HomeScreen = () => {
     });
   }, []);
 
+  const customButton = (onConfirm) => {
+    return (
+      <Button
+        onPress={onConfirm}
+        style={{
+          container: { width: "80%", marginHorizontal: "3%" },
+          text: { fontSize: 20 },
+        }}
+        primary
+        title="Submit"
+      />
+    );
+  };
+
   return (
     <View>
       <Header />
+      <ScrollView>
+        <View
+          style={{
+            margin: 20,
+            borderColor: "#FFC72C",
+            borderWidth: 3,
+            borderRadius: 6,
+          }}
+        >
+          <Pressable style={{flexDirection:"row", alignItems:"center", gap:10, paddingHorizontal:10, borderColor:"#FFC72C", borderWidth:2, paddingVertical:15}}>
+            <Ionicons name="search" size={24} color="black" />
+            <TextInput placeholder="Enter your Destination" />
+          </Pressable>
+          <Pressable style={{flexDirection:"row", alignItems:"center", gap:10, paddingHorizontal:10, borderColor:"#FFC72C", borderWidth:2, paddingVertical:15}}>
+          <Feather name="calendar" size={24} color="black" />
+          <DatePicker
+                style={{
+                  width: 350,
+                  height: 30,
+                  borderRadius: 0,
+                  borderWidth: 0,
+                  borderColor: "transparent",
+                }}
+                customStyles={{
+                  placeholderText: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  },
+                  headerStyle: {
+                    backgroundColor: "#003580",
+                  },
+                  contentText: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  },
+                }}
+               
+              />
+          </Pressable>
+          <Pressable style={{flexDirection:"row", alignItems:"center", gap:10, paddingHorizontal:10, borderColor:"#FFC72C", borderWidth:2, paddingVertical:15}}></Pressable>
+          <Pressable style={{flexDirection:"row", alignItems:"center", gap:10, paddingHorizontal:10, borderColor:"#FFC72C", borderWidth:2, paddingVertical:15}}></Pressable>
+        </View>
+      </ScrollView>
     </View>
   );
 };
